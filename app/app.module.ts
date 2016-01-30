697c0eef5e31191cc8e1e79800b5495dae5d4d5f
Ayman <ayman.farhat1@gmail.com>
2016-01-30 21:04:03 +0200

    Simple toggle for intro view and table view

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,26 +3,44 @@ import {DataModule} from './data.module.js';
 export class AppModule {
     constructor() { 
         let dataModule = new DataModule(),
-            uiModule  = new UIModule();
+            uiModule  = new UIModule(dataModule);
     }
 }
 
 export class UIModule {
     sampleDataBtn : HTMLElement;
+    introContainer: HTMLElement;
+    dataContainer: HTMLElement;
 
-    constructor() { 
+    constructor(dataService: DataModule) { 
         this.sampleDataBtn = document.getElementById('fetchSampleDataBtn');
+        this.introContainer = document.getElementById('introContainer');
+        this.dataContainer = document.getElementById('dataContainer');
+        this.dataService = dataService;
 
-        this.sampleDataBtn.addEventListener('click', function (e) {
-            let dataModule = new DataModule();
-
-            dataModule.getSampleData().then(function(response) {
-                console.log(dataModule.parseAccessLogData(response));
-            }, function (error) {
-                console.log(error);
-            });
-        });
+        this.sampleDataBtn.addEventListener('click', this.dataBtnClick.bind(this));
     }
+
+    toggleDataView(state: Boolean) {
+        this.introContainer.style.display = (state) ? 'none' : 'block';
+        this.dataContainer.style.display = (state) ? 'block' : 'none';
+    };
+
+    renderDataTable(data: Array) {
+        console.log(data); 
+    }
+
+    dataBtnClick() {
+        this.dataService.getSampleData().then((response) => {
+            let parsedLog = this.dataService.parseAccessLogData(response);
+
+            this.toggleDataView(true);
+            this.renderDataTable(parsedLog);
+
+        }, function (error) {
+            console.log(error);
+        });
+    };
 }
 
 let app = new AppModule();
